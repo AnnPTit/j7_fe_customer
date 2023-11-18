ec2a5158622488998f0b2751b9881aba9c74adb9
quangnhph22872 <quangnhph22872@fpteduvn>
2023-11-18 15:00:10 +0700

    chuppachup

--- src/Components/gallery/Cards.js
+++ src/Components/gallery/Cards.js
@@ -1,34 +1,45 @@
-import React, { useState } from "react"
+import React, { useState } from "react";
 
 const Cards = (props) => {
-  const [popup, setPopup] = useState(false)
+  const [popup, setPopup] = useState(false);
 
   const toggleModal = () => {
-    setPopup(!popup)
-  }
+    setPopup(!popup);
+  };
+
   return (
     <>
-      <div className='items'>
-        <div className='img'>
-          <img src={props.imgaes} alt='Gallery Image' />
-          <i className='fas fa-image' onClick={toggleModal}></i>
+      <div className="items">
+        <div className="img">
+          <img
+            src={props.imgaes}
+            alt="Gallery Image"
+            onClick={toggleModal}
+            style={{ cursor: "pointer" }}
+          />
+          <i className="fas fa-image" onClick={toggleModal}></i>
         </div>
-        <div className='title'>
-          <h3>{props.title} </h3>
+        <div className="title">
+          <h3>{props.title}</h3>
+          <p>Price: ${props.price}</p>
         </div>
       </div>
 
       {popup && (
-        <div className='popup'>
-          <div className='hide'></div>
-          <div className='popup-content'>
+        <div className="popup">
+          <div className="hide" onClick={toggleModal}></div>
+          <div className="popup-content">
             <button onClick={toggleModal}>Close</button>
-            <img src={props.imgaes} alt='Gallery Image' />
+            <img
+              src={props.imgaes}
+              alt="Gallery Image"
+              style={{ maxWidth: "100%", maxHeight: "80vh", margin: "auto", display: "block" }}
+            />
           </div>
         </div>
       )}
     </>
-  )
-}
+  );
+};
 
-export default Cards
+export default Cards;
