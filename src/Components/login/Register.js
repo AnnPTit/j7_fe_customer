dd012a5eb954be48963b4e942f18da6e16100b6d
tuann1 <tuannvph27234@fpt.edu.vn>
2023-11-23 20:52:02 +0700

    dang ky

--- src/Components/login/Register.js
+++ src/Components/login/Register.js
@@ -1,72 +1,132 @@
-import React, { useState } from "react"
-import HeadTitle from "../../Common/HeadTitle/HeadTitle"
-import "./design.css"
+import React, { useState } from "react";
+import HeadTitle from "../../Common/HeadTitle/HeadTitle";
+import axios from "axios";
+import "./design.css";
 
 const Register = () => {
-  const [name, setName] = useState("")
-  const [email, setEmail] = useState("")
-  const [password, setPassword] = useState("")
-  const [cpassword, setCpassword] = useState("")
+  const [fullname, setFullname] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [cpassword, setCpassword] = useState("");
+  const [recValue, setRecValue] = useState([]);
+  const [loading, setLoading] = useState(false); // Thêm trạng thái loading
 
-  const [recValue, setRecValue] = useState([])
-  const submitForm = (e) => {
-    e.preventDefault()
-    const newValue = { name: name, email: email, password: password, cpassword: cpassword }
+  const submitForm = async (e) => {
+    e.preventDefault();
 
-    setRecValue([...recValue, newValue])
-    console.log(newValue)
+    // Kiểm tra mật khẩu và xác nhận mật khẩu
+    if (cpassword !== password) {
+      console.error("Mật khẩu không trùng khớp!");
+      // Xử lý khi mật khẩu và xác nhận mật khẩu không khớp, có thể hiển thị thông báo cho người dùng
+      return;
+    }
 
-    setName("")
-    setEmail("")
-    setPassword("")
-    setCpassword("")
-  }
+    setLoading(true);
+
+    try {
+      const newValue = {
+        fullname: fullname,
+        email: email,
+        password: password,
+      };
+
+      // Gọi API để lưu thông tin người dùng mới
+      const response = await axios.post(
+        "http://localhost:2003/api/home/customer/save",
+        newValue
+      );
+
+      console.log(response);
+
+      if (response.status === 200) {
+        console.log("API call successful");
+        // Chuyển hướng sau khi đăng ký thành công
+        window.location.href = "/sign-in";
+      } else {
+        console.log("API call failed");
+        // Xử lý khi API gặp lỗi, có thể hiển thị thông báo cho người dùng
+      }
+    } catch (error) {
+      console.error("API call failed:", error);
+      // Xử lý khi có lỗi xảy ra trong quá trình gọi API, có thể hiển thị thông báo cho người dùng
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
     <>
       <HeadTitle />
-      <section className='forms top'>
-        <div className='container'>
-          <div className='sign-box'>
-            <p>Don't have an account? Create your account, it takes less than a minute.</p>
-            <form action='' onSubmit={submitForm}>
-              <input type='text' name='name' value={name} onChange={(e) => setName(e.target.value)} placeholder='Name' required />
-              <input type='email' name='email' value={email} onChange={(e) => setEmail(e.target.value)} placeholder='Email' required />
-              <input type='password' name='password' value={password} onChange={(e) => setPassword(e.target.value)} placeholder='Password' required />
-              <input type='password' name='cpassword' value={cpassword} onChange={(e) => setCpassword(e.target.value)} placeholder='Confirm Password' required />
+      <section className="forms top">
+        <div className="container">
+          <div className="sign-box">
+            <p>
+              Don't have an account? Create your account, it takes less than a
+              minute.
+            </p>
+            <form action="" onSubmit={submitForm}>
+              <input
+                type="text"
+                name="name"
+                value={fullname}
+                onChange={(e) => setFullname(e.target.value)}
+                placeholder="Name"
+                required
+              />
+              <input
+                type="email"
+                name="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                placeholder="Email"
+                required
+              />
+              <input
+                type="password"
+                name="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                placeholder="Password"
+                required
+              />
+              <input
+                type="password"
+                name="cpassword"
+                value={cpassword}
+                onChange={(e) => setCpassword(e.target.value)}
+                placeholder="Confirm Password"
+                required
+              />
 
-              <button type='submit' className='primary-btn'>
-                Create an Account
+              <button type="submit" className="primary-btn" disabled={loading}>
+                {loading ? "Creating Account..." : "Create an Account"}
               </button>
             </form>
           </div>
         </div>
       </section>
 
-      <section className='show-data'>
-        {recValue.map((cureentValue) => {
-          return (
-            <>
-              <div className='sign-box'>
-                <h1>Create an Account Successfully</h1>
-                <h3>
-                  Name : <p>{cureentValue.name}</p>
-                </h3>
-                <h3>
-                  Email : <p>{cureentValue.email}</p>
-                </h3>
-                <h3>
-                  Password : <p>{cureentValue.password}</p>
-                </h3>
-                <h3>
-                  Confirm Password : <p>{cureentValue.cpassword}</p>
-                </h3>
-              </div>
-            </>
-          )
-        })}
+      <section className="show-data">
+        {recValue.map((currentValue, index) => (
+          <div className="sign-box" key={index}>
+            <h1>Create an Account Successfully</h1>
+            <h3>
+              Name : <p>{currentValue.name}</p>
+            </h3>
+            <h3>
+              Email : <p>{currentValue.email}</p>
+            </h3>
+            <h3>
+              Password : <p>{currentValue.password}</p>
+            </h3>
+            <h3>
+              Confirm Password : <p>{currentValue.cpassword}</p>
+            </h3>
+          </div>
+        ))}
       </section>
     </>
-  )
-}
+  );
+};
 
-export default Register
+export default Register;
